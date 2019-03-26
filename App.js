ff3985350746aaa6e5265ba693bf434b4ed7edda
mitsuyoshi <m123matsuo@icloud.com>
2019-03-26 22:28:11 +0900

    Edit Buttons

--- App.js
+++ App.js
@@ -4,15 +4,156 @@ import {
   Text,
   View,
   Platform,
+  TouchableOpacity,
 } from 'react-native';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
+// ボタンのFunctional Component
+const CalcButton = (props) => {
+  const flex = props.flex ? props.flex : 1
+  return (
+    <TouchableOpacity
+      style={[styles.calcButton, {flex, flex}]}
+      onPress={() => {props.btnEvent()}}>
+      <Text style={styles.calcButtonText}>{props.label}</Text>
+    </TouchableOpacity>
+  )
+}
+
+// ボタンのFragmentを返すFunctional Component
+const CalcButtons = (props) => {
+  return (
+    <React.Fragment>
+      { props.buttons.map(button => {
+        return (
+          <CalcButton
+            key={button.label}
+            flex={button.flex}
+            label={button.label}
+            btnEvent={button.btnEvent}
+          />
+        )
+      })}
+    </React.Fragment>
+  )
+}
+
 export default class App extends React.Component {
+  // ボタンの定義
+  buttons = [
+    [
+      {
+        label: 'AC',
+        flex: 2,
+        btnEvent: () => {this.acButton()},
+      },
+      {
+        label: 'C',
+        btnEvent: () => {this.cButton()},
+      },
+      {
+        label: '+',
+        btnEvent: () => {this.calcButton('+')},
+      },
+    ],
+    [
+      {
+        label: '7',
+        btnEvent: () => {this.valueButton('7')},
+      },
+      {
+        label: '8',
+        btnEvent: () => {this.valueButton('8')},
+      },
+      {
+        label: '9',
+        btnEvent: () => {this.valueButton('9')},
+      },
+      {
+        label: '-',
+        btnEvent: () => {this.valueButton('-')},
+      },
+    ],
+    [
+      {
+        label: '4',
+        btnEvent: () => {this.valueButton('4')},
+      },
+      {
+        label: '5',
+        btnEvent: () => {this.valueButton('5')},
+      },
+      {
+        label: '6',
+        btnEvent: () => {this.valueButton('6')},
+      },
+      {
+        label: '*',
+        btnEvent: () => {this.valueButton('*')},
+      },
+    ],
+    [
+      {
+        label: '1',
+        btnEvent: () => {this.valueButton('1')},
+      },
+      {
+        label: '2',
+        btnEvent: () => {this.valueButton('2')},
+      },
+      {
+        label: '3',
+        btnEvent: () => {this.valueButton('3')},
+      },
+    ],
+    [
+      {
+        label: '0',
+        btnEvent: () => {this.valueButton('0')},
+      },
+      {
+        label: '.',
+        btnEvent: () => {this.valueButton('.')},
+      },
+      {
+        label: '/',
+        btnEvent: () => {this.valueButton('/')},
+      },
+    ],
+    [
+      {
+        label: 'Enter',
+        btnEvent: () => {this.enterButton()}
+      }
+    ]
+  ]
+
+  // ボタンの役割ごとに関数作成
+  valueButton = (value) => {
+
+  }
+
+  enterButton = () => {
+
+  }
+
+  calcButton = (value) => {
+
+  }
+
+  acButton = () => {
+
+  }
+
+  cButton = () => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        { /* 1: 結果を表示するView */ }
+        { /* 結果を表示するView */ }
         <View style={styles.results}>
           <View style={styles.resultLine}>
           </View>
@@ -21,22 +162,29 @@ export default class App extends React.Component {
           <View style={styles.resultLine}>
           </View>
         </View>
-        { /* 2: ボタンを配置するView */ }
+        { /* ボタンを配置するView */ }
         <View style={styles.buttons}>
           <View style={styles.buttonsLine}>
+            { /* CalcButtonsでそれぞれのView に配置していく */ }
+            <CalcButtons buttons={this.buttons[0]} />
           </View>
           <View style={styles.buttonsLine}>
+            <CalcButtons buttons={this.buttons[1]} />
           </View>
           <View style={styles.buttonsLine}>
+            <CalcButtons buttons={this.buttons[2]} />
           </View>
           <View style={styles.lastButtonLinesContainer}>
             <View style={styles.twoButtonLines}>
               <View style={styles.buttonsLine}>
+                <CalcButtons buttons={this.buttons[3]} />
               </View>
               <View style={styles.buttonsLine}>
+                <CalcButtons buttons={this.buttons[4]} />
               </View>
             </View>
             <View style={styles.enterButtonContainer}>
+              <CalcButtons buttons={this.buttons[5]} />
             </View>
           </View>
         </View>
@@ -50,7 +198,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop: STATUSBAR_HEIGHT,
   },
-  // 3: 結果を表示する領域と、一つずつの行のスタイル
+  // 結果を表示する領域と、一つずつの行のスタイル
   results: {
     flex: 3,
     backgroundColor: '#fff',
@@ -61,7 +209,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'flex-end',
   },
-  // 4: ボタンを表示する領域と、ボタンの行のスタイル
+  // ボタンを表示する領域と、ボタンの行のスタイル
   buttons: {
     flex: 5,
   },
@@ -73,7 +221,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 1,
   },
-  // 5: 最後の二行は組み方が違うので違うスタイルを設定する
+  // 最後の二行は組み方が違うので違うスタイルを設定する
   lastButtonLinesContainer: {
     flex: 2,
     flexDirection: 'row',
@@ -85,5 +233,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     borderWidth: 1,
+  },
+  calcButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    borderWidth: 1,
+    borderColor: "#b0c4de",
+  },
+  calcButtonText: {
+    fontSize: 20,
   },
 });
